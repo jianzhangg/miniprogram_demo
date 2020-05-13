837a0bc5ff785f211014a2126e098422fdc4b688
jianzhangg <jian.zhangg@qq.com>
2020-05-13 17:44:58 +0800

    progress 组件

---
+++ pages/ccomponents_content_demo/progress_demo/progress_demo.js
@@ -0,0 +1,17 @@
+Page({
+    data: {
+        percentValue: 0
+    },
+    onLoad() {
+        let timer;
+        timer = () => {
+            setTimeout(() => {
+                const value = this.data.percentValue;
+                this.setData({
+                    percentValue: value < 100 ? value + 10 : value
+                });
+                timer()
+            }, 2000)
+        }
+    }
+})
